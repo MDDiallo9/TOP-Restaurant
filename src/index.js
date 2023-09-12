52307cc22a8960d11cac0a0764796538c3b34be2
Mohamed WLaptop <114567817+MDDiallo9@users.noreply.github.com>
2023-09-12 13:02:07 +0200

    WIP needs a lot of clean up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./style.css"
+import logo from "./restaurant-logo.png"
+import contact, { createContact } from "./contactmodule.js"
+import { createCarte } from "./cartemodule"
+import { createHome } from "./homemodule"
 
-const elementCreator = (type,content,classname) => {
+const elementCreator = (type,content,classname="") => {
     let element = document.createElement(type)
     for (let item of classname){
         console.log(item) 
@@ -10,11 +14,54 @@ const elementCreator = (type,content,classname) => {
     return element
 }
 
-console.log("test");
-let testDiv = elementCreator("div","petit test",["blue","flex"])
-console.log(testDiv)
-
-
 let container = document.querySelector("#content")
-container.appendChild(testDiv)
+let logoimg = document.querySelector("#logoimg")
+
+logoimg.src = logo
+/* createContact()
+createCarte() */
+/* createHome() */
+
+const cleanContainer = () => {
+    for (let elem of container.children){
+        container.removeChild(elem)
+    }
+}
+
+createContact()
+
+const home = document.querySelector('#home')
+const navButttons = document.querySelectorAll("li")
+
+const btnNames = ["home","carte","sens","traiteur","contact"]
+for (let i = 0 ; i < btnNames.length ; i++){
+    navButttons[i].setAttribute("id",btnNames[i])
+}
+const runSelectors = () => {
+    document.querySelector("#home").addEventListener("click", () => {
+        createHome()
+    })
+    document.querySelector("#carte").addEventListener("click", () => {
+        createCarte()
+    })
+    document.querySelector("#sens").addEventListener("click", () => {
+        createHome()
+    })
+    document.querySelector("#traiteur").addEventListener("click", () => {
+        createHome()
+    })
+    document.querySelector("#contact").addEventListener("click", () => {
+        createContact()
+    })
+}
+
+runSelectors()
+
+export {elementCreator,cleanContainer,container}
+
+
+
+
+
+
 
